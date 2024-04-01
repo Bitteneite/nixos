a0f343f799526079843d6c86de6f039b252fdea9
Bitteneite <5614646+Bitteneite@users.noreply.github.com>
2024-04-01 12:18:43 +0400

    Simplified and removed comments

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -5,22 +5,16 @@
   ./../../modules/zsh.nix
   ./../../modules/file-manager/nnn.nix
   ];
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+  
+  # User info
   home.username = "bitteneite";
   home.homeDirectory = "/home/bitteneite";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
+
+  # You should not change this value
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
+  # Nix Packages to install
   home.packages = with pkgs; [
     
     # Terminal Stuff
@@ -36,45 +30,9 @@
     # # fonts?
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-  
-  
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/bitteneite/etc/profile.d/hm-session-vars.sh
-  #
 
   # Kitty: Terminal Emulator
   programs.kitty = {
@@ -88,22 +46,7 @@
     font.size = 14;
   };
 
-  #
-  # xdg.configFile."lf/icons".source = ./icons;
-  #
-  # programs.lf = {
-  #   
-  #   enable = true;
-  #
-  #   settings = {
-  #     preview = true;
-  #     hidden = true;
-  #     drawbox = true;
-  #     icons = true;
-  #     ignorecase = true;
-  #   };
-  # };
-
+  # Defaults
   home.sessionVariables = {
     EDITOR = "nvim";
   };
