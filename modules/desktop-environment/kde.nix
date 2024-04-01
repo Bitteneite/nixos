f90d3d53a99f4721ff1915730103fc7274b30f33
Bitteneite <5614646+Bitteneite@users.noreply.github.com>
2024-04-01 20:45:04 +0400

    Added KDE module

---
+++ modules/desktop-environment/kde.nix
@@ -0,0 +1,19 @@
+# To be imported in configuration.nix
+{ config, pkgs, users, ... }:
+
+{
+  # KDE Plasma: Desktop Environment
+  
+  #X11 windowing system
+  services.xserver = {
+    enable = true;
+	displayManager.sddm.enable = true;
+	desktopManager.plasma5.enable = true;
+	layout = "us";
+	xkbVariant = "";
+	
+	#displayManager.autoLogin.enable = true;
+	#displayManager.autoLogin.user = "bitteneite";
+	
+  }
+}
